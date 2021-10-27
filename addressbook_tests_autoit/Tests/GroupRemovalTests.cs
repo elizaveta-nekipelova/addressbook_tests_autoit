36e2e99b848206a841deaca3389929fdf3243b60
Elizaveta Brezgunova <elizaveta.brezgunova@euromoneyplc.com>
2021-10-27 22:53:21 +0300

    Add project files.

---
+++ addressbook_tests_autoit/Tests/GroupRemovalTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            int index = 0;
+
+            app.Groups.Delete(index);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(index);
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
